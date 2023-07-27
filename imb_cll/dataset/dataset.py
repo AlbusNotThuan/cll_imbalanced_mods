111a93b95353f7ebb89085dd174da1e46b26c9ea
maitanha <maitanhaksdtvt6@gmail.com>
2023-07-27 16:06:24 +0700

    add more datasets
    Add more datasets: KMNIST, FashionMNIST, MNIST

--- imb_cll/dataset/dataset.py
+++ imb_cll/dataset/dataset.py
@@ -1,5 +1,6 @@
 # from .cl_cifar import CLCIFAR10
-from .clcifar_cluster_label import CLCIFAR10
+from .clcifar_cluster_label import CLCIFAR20, CLCIFAR10, CLCIFAR100
+from .clmnist_cluster_label import CLMNIST, CLFashionMNIST, CLKMNIST
 # from torch.utils.data import DataLoader, Dataset
 
 def prepare_dataset(dataset, data_type, max_train_samples=None, multi_label=False, augment=False, imb_type=None, imb_factor=1.0):
@@ -24,7 +25,7 @@ def prepare_dataset(dataset, data_type, max_train_samples=None, multi_label=Fals
 
 def prepare_cluster_dataset(dataset, data_type=None, kmean_cluster= None, max_train_samples=None, multi_label=False, augment=False, imb_type=None, imb_factor=1.0, pretrain=None):
 
-    if dataset == "cifar10":
+    if dataset == "CIFAR10":
         if data_type == "train":
             dataset = CLCIFAR10(
                 root="./data/cifar10",
@@ -37,28 +38,85 @@ def prepare_cluster_dataset(dataset, data_type=None, kmean_cluster= None, max_tr
                 augment=augment,
                 imb_type=imb_type,
                 imb_factor=imb_factor,
-                pretrain=pretrain
+                pretrain=pretrain,
+                dataset=dataset,
             )
         else:
             dataset = CLCIFAR10(root="./data/cifar10", train=False, data_type=data_type)
+    elif dataset == "CIFAR20":
+        if data_type == "train":
+            dataset = CLCIFAR20(
+                root="./data/cifar20",
+                train=True,
+                data_type=data_type,
+                download=True,
+                kmean_cluster=kmean_cluster,
+                max_train_samples=max_train_samples,
+                multi_label=multi_label,
+                augment=augment,
+                imb_type=imb_type,
+                imb_factor=imb_factor,
+                pretrain=pretrain,
+                dataset=dataset,
+            )
+        else:
+            dataset = CLCIFAR20(root="./data/cifar20", train=False, data_type=data_type)
+    elif dataset == "MNIST":
+        if data_type == "train":
+            dataset = CLMNIST(
+                root="./data/mnist",
+                train=True,
+                data_type=data_type,
+                download=True,
+                kmean_cluster=kmean_cluster,
+                max_train_samples=max_train_samples,
+                multi_label=multi_label,
+                augment=augment,
+                imb_type=imb_type,
+                imb_factor=imb_factor,
+                pretrain=pretrain,
+                dataset=dataset,
+            )
+        else:
+            dataset = CLMNIST(root="./data/mnist", train=False, data_type=data_type)
+    elif dataset == "FashionMNIST":
+        if data_type == "train":
+            dataset = CLFashionMNIST(
+                root="./data/FashionMNIST",
+                train=True,
+                data_type=data_type,
+                download=True,
+                kmean_cluster=kmean_cluster,
+                max_train_samples=max_train_samples,
+                multi_label=multi_label,
+                augment=augment,
+                imb_type=imb_type,
+                imb_factor=imb_factor,
+                pretrain=pretrain,
+                dataset=dataset,
+            )
+        else:
+            dataset = CLFashionMNIST(root="./data/FashionMNIST", train=False, data_type=data_type)
+
+    elif dataset == "KMNIST":
+        if data_type == "train":
+            dataset = CLKMNIST(
+                root="./data/KMNIST",
+                train=True,
+                data_type=data_type,
+                download=True,
+                kmean_cluster=kmean_cluster,
+                max_train_samples=max_train_samples,
+                multi_label=multi_label,
+                augment=augment,
+                imb_type=imb_type,
+                imb_factor=imb_factor,
+                pretrain=pretrain,
+                dataset=dataset,
+            )
+        else:
+            dataset = CLKMNIST(root="./data/KMNIST", train=False, data_type=data_type)
     else:
         raise NotImplementedError
-    # import pdb
-    # pdb.set_trace()
-    return dataset, dataset.num_classes
-
-# class CLCustomDataset(Dataset):
-#     def __init__(self, data, targets, true_targets):
-#         self.data = data
-#         self.targets = targets
-#         self.true_targets = true_targets
-#         print('Init CLCustomDataset')
-#         breakpoint()
-
-#     def __len__(self):
-#         return len(self.data)
     
-#     def __getitem__(self, index):
-#         print('getitem')
-#         breakpoint()
-#         return self.data[index], self.targets[index], self.true_targets[index]
+    return dataset, dataset.input_dim, dataset.num_classes
--- imb_cll/utils/cl_augmentation.py
+++ imb_cll/utils/cl_augmentation.py
@@ -24,7 +24,6 @@ def mixup_cl_data(x, y, ytrue, device, alpha=1.0):
         lam = np.random.beta(alpha, alpha)
     else:
         lam = 1
-    
     batch_size = x.size()[0]
     mixed_x = torch.zeros_like(x).to(device)
     y_a, y_b = torch.zeros_like(y).to(device), torch.zeros_like(y).to(device)  #to(device)
@@ -37,8 +36,10 @@ def mixup_cl_data(x, y, ytrue, device, alpha=1.0):
                 break
     return mixed_x, y_a, y_b, lam
 
-def aug_intra_class(x, y, ytrue, device, alpha=1.0):
+def aug_intra_class(x, y, ytrue, k_cluster_label, device, alpha=1.0):
     '''Returns mixed inputs, pairs of targets, and lambda'''
+    count_error = 0
+
     if alpha > 0:
         lam = np.random.beta(alpha, alpha)
     else:
@@ -52,11 +53,16 @@ def aug_intra_class(x, y, ytrue, device, alpha=1.0):
             j = random.randint(0, batch_size - 1)
             # import pdb
             # pdb.set_trace()                                                            
-            if ytrue[i] == ytrue[j]:
+            if k_cluster_label[i] == k_cluster_label[j]:
                 mixed_x[i] = lam * x[i] + (1 - lam) * x[j]
                 y_a[i], y_b[i] = y[i], y[j]
+
+                # Count the violent case when true label appearing in cl label
+                if y_a[i] == ytrue[j] or y_b[i] == ytrue[j]:
+                    count_error += 1
+
                 break
-    return mixed_x, y_a, y_b, lam
+    return mixed_x, y_a, y_b, lam, count_error
 
 def mixup_criterion(criterion, pred, y_a, y_b, lam):
     y_a = y_a.squeeze()
